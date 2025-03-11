603b46ec813b4650a8ffb2593944d4cfa9627e58
diegopessoa <diegopessoa160@gmail.com>
2025-03-10 20:25:50 -0400

    atualizando

---
+++ src/ContaBancaria.java
@@ -0,0 +1,38 @@
+public class ContaBancaria {
+    // Atributos da conta bancária
+    private int numeroConta;
+    private String agencia;
+    private String nomeCliente;
+    private double saldo;
+
+    // Construtor para inicializar os atributos
+    public ContaBancaria(int numeroConta, String agencia, String nomeCliente, double saldo) {
+        this.numeroConta = numeroConta;
+        this.agencia = agencia;
+        this.nomeCliente = nomeCliente;
+        this.saldo = saldo;
+    }
+
+    // Métodos getters (para acessar os atributos)
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    // Método para exibir as informações da conta
+    public void exibirInformacoes() {
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, "
+                + "sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque.");
+    }
+}
